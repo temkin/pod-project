87a9f3e8d6ca1111db73b78b48347462bb24924b
zaa-anadea <zaa@anadeainc.com>
2024-12-16 17:25:07 +0200

    feat: add HomeScreen and setup react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { registerSW } from "virtual:pwa-register";
+import App from "./app";
 
 const updateSW = registerSW({
   onNeedRefresh() {
