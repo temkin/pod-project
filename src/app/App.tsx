037b7180ddc44699875f17f6837ace46801cb570
zaa-anadea <zaa@anadeainc.com>
2024-12-17 19:02:16 +0200

    feat: add github workflow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,14 @@ import { BrowserRouter } from "react-router";
 import Root from "./Root";
 import { Provider } from "react-redux";
 import store from "../store";
+import { MODES } from "../env";
 
 const App = () => {
+  const basename = import.meta.env.MODE === MODES.PROD ? `/pod-project` : "/";
+
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename={basename}>
         <Root />
       </BrowserRouter>
     </Provider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,56 @@
-import { defineConfig } from "vite";
+import { defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
-      manifest: {
-        name: "Barcode Scanner PWA",
-        short_name: "Scanner",
-        description: "Barcode Scanner Application",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "vite.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "vite.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
+export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: "autoUpdate",
+        includeAssets: [
+          "favicon.ico",
+          "apple-touch-icon.png",
+          "masked-icon.svg",
         ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
-      },
-    }),
-  ],
+        manifest: {
+          name: "Barcode Scanner PWA",
+          short_name: "Scanner",
+          description: "Barcode Scanner Application",
+          theme_color: "#ffffff",
+          icons: [
+            {
+              src: "vite.png",
+              sizes: "192x192",
+              type: "image/png",
+            },
+            {
+              src: "vite.png",
+              sizes: "512x512",
+              type: "image/png",
+              purpose: "any maskable",
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        },
+      }),
+    ],
+  };
+
+  if (mode === "development") {
+    config.server = {
+      host: true,
+      port: 5173,
+    };
+
+    config.base = "/";
+  }
+
+  if (mode === "production") {
+    config.base = "/pod-project/";
+  }
+
+  return config;
 });
