d66a054c1feea5fd497b8a44a3f35b2cce0f4491
zaa-anadea <zaa@anadeainc.com>
2024-12-20 13:31:44 +0200

    feat: add notifications for scanner errors (#19)

--- src/screens/ScanScreen/ScanScreen.tsx
+++ src/screens/ScanScreen/ScanScreen.tsx
@@ -1,21 +1,30 @@
 import { Box, Grid2 as Grid } from "@mui/material";
 import { useNavigate } from "react-router";
+import { useNotifications } from "@toolpad/core";
 import { ROUTES } from "../../app";
 import { CodeScanner, Layout, ScanningInstructions } from "../../components";
 import { ScanResult } from "../../components/types";
+import { NOTIFICATIONS_SEVERITIES } from "../../lib";
 
 const ScanScreen = () => {
   const navigate = useNavigate();
+  const notifications = useNotifications();
 
   const handleScan = (result: ScanResult) => {
     navigate(ROUTES.SIGNATURE_CODE(result.code));
+  };
+
+  const handleError = (error: Error) => {
+    notifications.show(error.message, {
+      severity: NOTIFICATIONS_SEVERITIES.ERROR,
+    });
   };
 
   return (
     <Layout name="Scanner">
       <Grid container spacing={2} height="calc(100vh - 64px)">
         <Grid size={12}>
-          <CodeScanner onScan={handleScan} />
+          <CodeScanner onScan={handleScan} onError={handleError} />
         </Grid>
 
         <Box
