456570ede9fcb3232886861cf3c7f199a1f1f519
zaa-anadea <zaa@anadeainc.com>
2024-12-20 12:33:30 +0200

    fix: update canvas position on small devices (#18)

--- src/screens/SignatureScreen/SignatureScreen.tsx
+++ src/screens/SignatureScreen/SignatureScreen.tsx
@@ -1,14 +1,14 @@
 import { Typography, Grid2 as Grid, Box, Button } from "@mui/material";
 import { useNavigate, useParams } from "react-router";
 import Barcode from "react-barcode";
-import { ROUTES } from "../../app";
-import { Layout, SignatureCanvas } from "../../components";
-import { useAppDispatch, createSignedCode, addSignedCode } from "../../store";
-import styles from "./styles";
 import {
   QrCodeOutlined as QrCodeOutlinedIcon,
   RefreshOutlined as RefreshOutlinedIcon,
 } from "@mui/icons-material";
+import { ROUTES } from "../../app";
+import { Layout, SignatureCanvas } from "../../components";
+import { useAppDispatch, createSignedCode, addSignedCode } from "../../store";
+import styles from "./styles";
 
 const SignatureScreen = () => {
   const params = useParams();
@@ -30,46 +30,48 @@ const SignatureScreen = () => {
 
   return (
     <Layout name="Sign code">
-      <Grid container spacing={1.5} sx={styles.gridContainer}>
-        <Grid container size={12} rowSpacing={1.5}>
-          <Grid
-            size={12}
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-            gap={0.5}
-          >
-            <QrCodeOutlinedIcon />
-            <Typography fontWeight={500} fontFamily="Poppins" fontSize={20}>
-              {barcodeCode}
-            </Typography>
-          </Grid>
-          <Grid size={12} display="flex" justifyContent="center">
-            <Button
-              onClick={handleRetry}
-              sx={{ textTransform: "none" }}
-              startIcon={<RefreshOutlinedIcon />}
+      <Box sx={styles.scrollableContainer}>
+        <Grid container spacing={1.5} sx={styles.gridContainer}>
+          <Grid container size={12} rowSpacing={1.5}>
+            <Grid
+              size={12}
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+              gap={0.5}
             >
-              Retry
-            </Button>
+              <QrCodeOutlinedIcon />
+              <Typography fontWeight={500} fontFamily="Poppins" fontSize={20}>
+                {barcodeCode}
+              </Typography>
+            </Grid>
+            <Grid size={12} display="flex" justifyContent="center">
+              <Button
+                onClick={handleRetry}
+                sx={styles.retryButton}
+                startIcon={<RefreshOutlinedIcon />}
+              >
+                Retry
+              </Button>
+            </Grid>
+            <Grid size={12} display="flex" justifyContent="center">
+              <Box sx={styles.barcodeContainer}>
+                <Barcode
+                  value={barcodeCode}
+                  width={1.5}
+                  height={50}
+                  fontSize={0}
+                  background="transparent"
+                />
+              </Box>
+            </Grid>
           </Grid>
-          <Grid size={12} display="flex" justifyContent="center">
-            <Box sx={styles.barcodeContainer}>
-              <Barcode
-                value={barcodeCode}
-                width={1.5}
-                height={50}
-                fontSize={0}
-                background="transparent"
-              />
-            </Box>
-          </Grid>
-        </Grid>
 
-        <Grid size={12}>
-          <SignatureCanvas onSave={handleSaveSignature} />
+          <Grid size={12} sx={styles.canvasContainer}>
+            <SignatureCanvas onSave={handleSaveSignature} />
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Layout>
   );
 };
