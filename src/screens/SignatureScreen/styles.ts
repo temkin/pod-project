613068a26d123894eebe39e780141e2e3a4efc92
zaa-anadea <zaa@anadeainc.com>
2024-12-20 15:29:00 +0200

    fix: make sign screen more responsive

--- src/screens/SignatureScreen/styles.ts
+++ src/screens/SignatureScreen/styles.ts
@@ -2,10 +2,10 @@ const styles = {
   gridContainer: {
     p: 2,
     flexDirection: "column",
-    justifyContent: "space-between",
     height: "100%",
-    display: "flex",
     minHeight: "min-content",
+    display: "flex",
+    justifyItems: "space-between",
   },
   title: {
     fontWeight: "bold",
@@ -15,13 +15,11 @@ const styles = {
     color: "#5F6D7E",
   },
   barcodeContainer: {
-    p: 2,
+    p: 4,
     mb: 2,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    width: 220,
-    height: 220,
     borderRadius: 10,
     borderWidth: 1,
     borderStyle: "solid",
