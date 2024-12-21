d9037a548a59970d0f53ff6cfe139cbc44ef39ba
zaa-anadea <zaa@anadeainc.com>
2024-12-22 00:16:25 +0200

    feat: add error notifications for signature canvas (#29)

--- src/components/SignatureCanvas/SignatureCanvas.tsx
+++ src/components/SignatureCanvas/SignatureCanvas.tsx
@@ -1,26 +1,17 @@
 import React from "react";
 import SignatureCanvasBase from "react-signature-pad-wrapper";
-import { Box, Button, Alert } from "@mui/material";
-import {
-  Clear as ClearIcon,
-  RestartAlt as RestartIcon,
-  Done as DoneIcon,
-} from "@mui/icons-material";
+import { Box, Button } from "@mui/material";
+import { Clear as ClearIcon, Done as DoneIcon } from "@mui/icons-material";
 import { useSignature } from "./useSignature";
 import styles from "./styles";
-
-interface SignatureCanvasProps {
-  onSave?: (signature: string) => void;
-  onError?: (error: Error) => void;
-  onClear?: () => void;
-}
+import { SignatureCanvasProps } from "./types";
 
 const SignatureCanvas: React.FC<SignatureCanvasProps> = ({
   onSave,
   onError,
   onClear,
 }) => {
-  const { sigPadRef, error, saveSignature, clearSignature } = useSignature({
+  const { sigPadRef, saveSignature, clearSignature } = useSignature({
     onSave,
     onError,
     onClear,
@@ -36,25 +27,6 @@ const SignatureCanvas: React.FC<SignatureCanvasProps> = ({
           }}
         />
       </Box>
-
-      {error && (
-        <Alert
-          severity="error"
-          sx={styles.alert}
-          action={
-            <Button
-              color="inherit"
-              size="small"
-              onClick={clearSignature}
-              startIcon={<RestartIcon />}
-            >
-              Try Again
-            </Button>
-          }
-        >
-          {error.message}
-        </Alert>
-      )}
 
       <Box sx={styles.buttonsContainer}>
         <Button
