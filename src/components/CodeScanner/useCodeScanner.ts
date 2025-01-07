ac8b77a7d423463c3818dd49a72c7946562a5b3b
zaa-anadea <zaa@anadeainc.com>
2025-01-07 12:13:41 +0200

    fix: turn on pure barcode (#36)

--- src/components/CodeScanner/useCodeScanner.ts
+++ src/components/CodeScanner/useCodeScanner.ts
@@ -1,7 +1,10 @@
 import { useState } from "react";
 import { BarcodeFormat } from "@zxing/library";
-import { useZxing, Result } from "react-zxing";
+import { useZxing, Result, DecodeHintType } from "react-zxing";
 import { UseCodeScannerOptions, UseCodeScannerReturn } from "./types";
+
+const hints = new Map();
+hints.set(DecodeHintType.PURE_BARCODE, true); 
 
 const useCodeScanner = (
   options: UseCodeScannerOptions = {}
@@ -43,13 +46,14 @@ const useCodeScanner = (
     constraints: {
       video: {
         facingMode: "environment",
-        width: { ideal: 1280 },
-        height: { ideal: 720 },
-        aspectRatio: { ideal: 1.777778 },
-        frameRate: { ideal: 15 },
+        width: { ideal: 1920 },
+        height: { ideal: 1080 },
+        aspectRatio: { ideal: 1.7777777778 },
+        frameRate: { ideal: 30 },
       },
     },
-    timeBetweenDecodingAttempts: 1000,
+    hints,
+    timeBetweenDecodingAttempts: 100,
   });
 
   const restartScanning = () => {
