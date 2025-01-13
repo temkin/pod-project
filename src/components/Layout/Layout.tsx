2608a73c1e58ebca06c326e9865de2600330edd7
zaa-anadea <zaa@anadeainc.com>
2025-01-13 15:59:01 +0200

    Revert "fix: update actual version (#45)"
    This reverts commit 1a2305a9c10a3057302598343b9f2ea37bea980c.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ children, name }: PropsWithChildren<LayoutProps>) => {
             typography="small"
             color="common.white"
           >
-            v0.1.4
+            v0.1.3
           </Typography>
         </Box>
 
